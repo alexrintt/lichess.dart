e8b9c6fa699bb3ad357e7144efdcad3409093573
Alex Rintt <51419598+alexrintt@users.noreply.github.com>
2023-03-31 20:53:27 -0300

    Add [streamBoardGameState] API signature and start [streamIncomingEvents]

--- packages/lichess_client/lib/src/models/tv.dart
+++ packages/lichess_client/lib/src/models/tv.dart
@@ -70,14 +70,27 @@ class LichessGamePlayer with _$LichessGamePlayer {
       _$LichessGamePlayerFromJson(json);
 }
 
+/// https://lichess.org/api#tag/Board/operation/boardGameStream
+@freezed
+class LichessVariant with _$LichessVariant {
+  const factory LichessVariant({
+    required LichessVariantKey key,
+    required String name,
+    required String short,
+  }) = _LichessVariant;
+
+  factory LichessVariant.fromJson(Map<String, dynamic> json) =>
+      _$LichessVariantFromJson(json);
+}
+
 /// https://lichess.org/api#tag/Games/operation/apiGamesUser
 @freezed
 class LichessGame with _$LichessGame {
   const factory LichessGame({
     required String id,
     required bool rated,
-    required LichessVariant variant,
-    required ChessSpeed speed,
+    required LichessVariantKey variant,
+    required LichessSpeed speed,
     required PerfType perf,
     required int createdAt,
     required int lastMoveAt,
@@ -93,6 +106,130 @@ class LichessGame with _$LichessGame {
 
   factory LichessGame.fromJson(Map<String, dynamic> json) =>
       _$LichessGameFromJson(json);
+}
+
+abstract class LichessBoardGameEvent {
+  const LichessBoardGameEvent({required this.type});
+
+  final String type;
+}
+
+/// https://lichess.org/api#tag/Board
+@freezed
+class LichessGamePerf with _$LichessGamePerf {
+  const factory LichessGamePerf({
+    String? name,
+  }) = _LichessGamePerf;
+
+  factory LichessGamePerf.fromJson(Map<String, dynamic> json) =>
+      _$LichessGamePerfFromJson(json);
+}
+
+/// https://lichess.org/api#tag/Board
+@freezed
+class LichessGameFullEvent
+    with _$LichessGameFullEvent
+    implements LichessBoardGameEvent {
+  const factory LichessGameFullEvent({
+    required String type,
+    required String id,
+    required LichessVariant variant,
+    required LichessSpeed speed,
+    required LichessGamePerf perf,
+    required bool rated,
+    required LichessGameEventPlayer white,
+    required LichessGameEventPlayer black,
+    required LichessGameStateEvent state,
+    @JsonKey(name: 'createdAt') required int createdAtInMilliseconds,
+    LichessGameClock? clock,
+    @Default('startpos') String initialFen,
+    String? tournamentId,
+  }) = _LichessGameFullEvent;
+
+  const LichessGameFullEvent._();
+
+  factory LichessGameFullEvent.fromJson(Map<String, dynamic> json) =>
+      _$LichessGameFullEventFromJson(json);
+
+  DateTime get createdAt =>
+      DateTime.fromMillisecondsSinceEpoch(createdAtInMilliseconds, isUtc: true);
+}
+
+/// https://lichess.org/api#tag/Board
+@freezed
+class LichessChatLineEvent
+    with _$LichessChatLineEvent
+    implements LichessBoardGameEvent {
+  const factory LichessChatLineEvent({
+    required String type,
+    required LichessChatLineRoom room,
+    required String username,
+    required String text,
+  }) = _LichessChatLineEvent;
+
+  const LichessChatLineEvent._();
+
+  factory LichessChatLineEvent.fromJson(Map<String, dynamic> json) =>
+      _$LichessChatLineEventFromJson(json);
+}
+
+/// https://lichess.org/api#tag/Board
+@freezed
+class LichessOpponentGoneEvent
+    with _$LichessOpponentGoneEvent
+    implements LichessBoardGameEvent {
+  const factory LichessOpponentGoneEvent({
+    required String type,
+    required bool gone,
+    int? claimWinInSeconds,
+  }) = _LichessOpponentGoneEvent;
+
+  const LichessOpponentGoneEvent._();
+
+  factory LichessOpponentGoneEvent.fromJson(Map<String, dynamic> json) =>
+      _$LichessOpponentGoneEventFromJson(json);
+}
+
+/// https://lichess.org/api#tag/Board
+@freezed
+class LichessGameEventPlayer with _$LichessGameEventPlayer {
+  const factory LichessGameEventPlayer({
+    required int aiLevel,
+    required String id,
+    required String name,
+    required int rating,
+    required bool provisional,
+    String? title,
+  }) = _LichessGameEventPlayer;
+
+  const LichessGameEventPlayer._();
+
+  factory LichessGameEventPlayer.fromJson(Map<String, dynamic> json) =>
+      _$LichessGameEventPlayerFromJson(json);
+}
+
+/// https://lichess.org/api#tag/Board
+@freezed
+class LichessGameStateEvent
+    with _$LichessGameStateEvent
+    implements LichessBoardGameEvent {
+  const factory LichessGameStateEvent({
+    required String type,
+    required String moves,
+    required int wtime,
+    required int btime,
+    required int winc,
+    required int binc,
+    required LichessGameStatus status,
+    ChessColor? winner,
+    bool? wdraw,
+    bool? bdraw,
+    bool? wtakeback,
+    bool? btakeback,
+  }) = _LichessGameStateEvent;
+
+  factory LichessGameStateEvent.fromJson(Map<String, dynamic> json) =>
+      _$LichessGameStateEventFromJson(json);
 }
 
 /// https://lichess.org/api#tag/TV/operation/tvFeed
@@ -114,6 +251,7 @@ class LichessGameClock with _$LichessGameClock {
     int? initial,
     int? increment,
     int? totalTime,
+    int? limit,
   }) = _LichessGameClock;
 
   factory LichessGameClock.fromJson(Map<String, dynamic> json) =>
@@ -158,6 +296,17 @@ enum LichessGameSort {
   final String raw;
 }
 
+/// https://lichess.org/api#tag/Games/operation/apiGamesUser
+@JsonEnum(valueField: 'raw')
+enum LichessChatLineRoom {
+  player('player'),
+  spectator('spectator');
+
+  const LichessChatLineRoom(this.raw);
+
+  final String raw;
+}
+
 /// https://lichess.org/api#tag/TV/operation/tvChannels
 @JsonEnum(valueField: 'raw')
 enum TvChannel {
@@ -188,7 +337,7 @@ enum TvChannel {
 
 /// https://lichess.org/api#tag/TV/operation/tvChannels
 @JsonEnum(valueField: 'raw')
-enum LichessVariant {
+enum LichessVariantKey {
   standard('standard'),
   chess960('chess960'),
   crazyhouse('crazyhouse'),
@@ -200,14 +349,14 @@ enum LichessVariant {
   threeCheck('threeCheck'),
   fromPosition('fromPosition');
 
-  const LichessVariant(this.raw);
+  const LichessVariantKey(this.raw);
 
   final String raw;
 }
 
 /// https://lichess.org/api#tag/TV/operation/tvChannels
 @JsonEnum(valueField: 'raw')
-enum ChessSpeed {
+enum LichessSpeed {
   ultraBullet('ultraBullet'),
   bullet('bullet'),
   blitz('blitz'),
@@ -215,7 +364,7 @@ enum ChessSpeed {
   classical('classical'),
   correspondence('correspondence');
 
-  const ChessSpeed(this.raw);
+  const LichessSpeed(this.raw);
 
   final String raw;
 }
---
+++ packages/lichess_client/lib/src/services/board_service.dart
@@ -0,0 +1,52 @@
+import '../../lichess_client.dart';
+
+/// {@template board}
+/// Play on Lichess with physical boards and third-party clients. Works with normal Lichess accounts. Engine play or assistance is [forbidden](https://lichess.org/page/fair-play).
+///
+/// ### Features
+///
+/// - [Stream incoming chess moves](https://lichess.org/api#operation/boardGameStream).
+/// - [Play chess moves](https://lichess.org/api#operation/boardGameMove).
+/// - [Read](https://lichess.org/api#operation/boardGameStream) and [write](https://lichess.org/api#operation/boardGameChatPost) in the player and spectator chats.
+/// - [Receive](https://lichess.org/api#operation/apiStreamEvent), [create](https://lichess.org/api#operation/challengeCreate) and [accept](https://lichess.org/api#operation/challengeAccept) (or [decline](https://lichess.org/api#operation/challengeDecline)) challenges.
+/// - [Abort](https://lichess.org/api#operation/boardGameAbort) and [resign](https://lichess.org/api#operation/boardGameResign) games.
+/// - Compatible with normal Lichess accounts.
+///
+/// https://lichess.org/api#tag/Board
+/// {@endtemplate}
+abstract class BoardService with CloseableMixin {
+  /// Interface for this client.
+  const BoardService();
+
+  /// Stream the events reaching a lichess user in real time as [ndjson](https://lichess.org/api#section/Introduction/Streaming-with-ND-JSON).
+  ///
+  /// An empty line is sent every 6 seconds for keep alive purposes.
+  ///
+  /// Each non-empty line is a JSON object containing a type field. Possible values are:
+  ///
+  /// - `gameStart` Start of a game.
+  /// - `gameFinish` Completion of a game.
+  /// - `challenge` A player sends you a challenge or you challenge someone.
+  /// - `challengeCanceled` A player cancels their challenge to you.
+  /// - `challengeDeclined` The opponent declines your challenge.
+  ///
+  /// When the stream opens, all current challenges and games are sent.
+  ///
+  /// https://lichess.org/api#tag/Board/operation/apiStreamEvent
+  Future<LichessBoardIncomingEvent> streamIncomingEvents();
+
+  /// Stream the state of a game being played with the Board API, as [ndjson](https://lichess.org/api#section/Introduction/Streaming-with-ND-JSON).
+  ///
+  /// Use this endpoint to get updates about the game in real-time, with a single request.
+  ///
+  /// Each line is a JSON object containing a type `field`. Possible values are:
+  ///
+  /// - `gamefull` Full game data. All values are immutable, except for the state field.
+  /// - `gameState` Current state of the game. Immutable values not included. Sent when a move is played, a draw is offered, or when the game ends.
+  /// - `chatLine` Chat message sent by a user in the `room` "player" or "spectator".
+  /// - `opponentGone` Whether the opponent has left the game, and how long before you can claim a win or draw.
+  /// - The first line is always of type `gameFull`.
+  ///
+  /// The server closes the stream when the game ends, or if the game has already ended.
+  Stream<LichessBoardGameEvent> streamBoardGameState({required String gameId});
+}
